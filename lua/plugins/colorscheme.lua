a27d5c1dd6c7832e2046ab030937f8bec2155206
Simon Antonius Lauer <simon.lauer@posteo.de>
2025-04-14 15:49:04 +0200

    chore: plugin updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,12 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require('catppuccin').setup {
+      integrations = {
+        notify = true,
+        noice = true,
+      },
+    }
     vim.cmd.colorscheme 'catppuccin-mocha'
   end,
 }
